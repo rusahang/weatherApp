6198c70d3690a4164ab1286dfb3c4611c1af3878
rusahang <rusahang@gmail.com>
2024-10-23 15:55:16 +0700

    first commit

---
+++ weather_project/weather/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+import urllib.request
+import json
+import datetime
+
+# Create your views here.
+
+def weather(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=9d5eb6530d38019fea5ebec886a177e6').read()
+
+        list_of_data = json.loads(source)
+
+        data = {
+            'country_code': str(list_of_data['name']) + ', '+ str(list_of_data['sys']['country']),
+            'coordinate': str(list_of_data['coord']['lon']) + ', ' + str(list_of_data['coord']['lat']),
+            'temp': int(list_of_data['main']['temp']),
+            'pressure':int(list_of_data['main']['pressure']),
+            'humidity':int(list_of_data['main']['humidity']),
+            'description': str.title(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+            'feels_like': int(list_of_data['main']['feels_like']),
+            'wind_speed': float(list_of_data['wind']['speed']),
+            'day': datetime.date.today
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, 'index.html', data)
